ed07f0c252d0fffc4657fa85273b576ea2d4745c
Zhen An <zhen.an@ZhenAns-MacBook-Pro.local>
2020-05-31 00:28:44 +0800

    3-Timer

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,3 +1,8 @@
+<script>
+    export let progress = 30;
+
+</script>
+
 <style>
     .progress-container {
         margin: 20px 0;
@@ -11,7 +16,7 @@
 </style>
 
 <div bp="offset-5@md 4@md 12@sm" class="progress-container">
-    <div class="progress-bar" style="width: 20%">
-        <span class="sr-only">%</span>
+    <div class="progress-bar" style="width: {progress}%">
+        <span class="sr-only">{progress}%</span>
     </div>
 </div>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,22 +1,44 @@
 <script>
-    import ProgressBar from "./ProgressBar.svelte"
+  import ProgressBar from "./ProgressBar.svelte";
+  const totalSecond = 6;
+  let secondLeft = totalSecond;
+  let isRunning = false;
+    $:progress = 100 * (totalSecond - secondLeft)/totalSecond;
+
+  function startTimer() {
+    let timer = setInterval(() => {
+        isRunning = true;
+      secondLeft -= 1;
+      if (secondLeft == 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondLeft = totalSecond;
+      }
+    }, 1000);
+  }
 </script>
 
 <style>
-    h2 {
-        margin: 0;
-    }
-    .start {
-        background-color: rgb(154, 73, 73);
-        width: 100%;
-        margin: 10px 0;
-    }
+  h2 {
+    margin: 0;
+  }
+  .start {
+    background-color: rgb(154, 73, 73);
+    width: 100%;
+    margin: 10px 0;
+  }
+  .start[disabled] {
+      background-color: rgb(194, 194, 194);
+      cursor: not-allowed;
+  }
 </style>
+<h1>{progress}</h1>
+<ProgressBar progress = {progress}/>
 
-<ProgressBar />
 <div bp="grid">
-    <h2>Seconds Left.</h2>
-
+  <h2>{secondLeft} Seconds Left.</h2>
 </div>
 
-<button class="start">Start</button>
+<div>
+  <button disabled={isRunning} class="start" on:click={startTimer}>Start</button>
+</div>
